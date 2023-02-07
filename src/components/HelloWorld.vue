a5a0fa86224a9d9e9117ca3cb8bd0ca3b2862a94
Hermann Caraballo <hermann.caraballo@gmail.com>
2023-02-07 13:58:21 -0400

    Fix bio.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,13 @@
     <img alt="Vue logo" src="../assets/logo.png" width="200">
     <h1>Hermann Caraballo</h1>
     <h3>VueJS Developer</h3>
-    <p class="mb-5">13 años de carrera profesional como desarrollador de los cuales 7 años como Frontend Developer, he tenido la oportunidad de realizar labores como Tech Leader y Project Manager.</p>
+    <p class="mt-3">13 años de carrera profesional como desarrollador web de los cuales 7 años como Frontend Developer, 
+      he tenido la oportunidad de realizar labores como Tech Leader y Project Manager.</p>
+    <p>Soy una persona dedicada, constantemente busco aprender, revisar, leer sobre lo más nuevo que va saliendo en tecnología 
+    y desarrollo; esto en algunas oportunidades me ha hecho mucho bien en el campo personal como académico y laboral inclusive.</p>
+    <p class=" mb-5">Estudié en la Universidad Central de Venezuela, Licienciatura en Computación y completé y aprobé el curso de 
+    IBM Advanced Career Education for e-business, además de una serie de cursos y tutoriales on-line (principalmente de Udemy y CodeSchools) 
+    que constantemente repaso y procuro actualizar para así mantenerme al día, esto ha logrado darme la experiencia con la que hoy en día cuento.</p>
   </div>
 </template>
 
@@ -27,14 +33,10 @@ body {
   justify-content: space-between;
   background-color: #FFF;
   min-height: 400px;
-  font-size: 1.5em;
 }
 h1 {
-  margin-top: 46px;
+  margin-top: 23px;
   align-self: center;
-}
-h3 {
-  margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
@@ -49,5 +51,10 @@ a {
 }
 img {
   align-self: center;
+}
+p {
+  width: 69%;
+  align-self: center;
+  font-size: 1.2em;
 }
 </style>
